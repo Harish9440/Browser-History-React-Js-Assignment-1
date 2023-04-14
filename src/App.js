94737b8109f618c08ba89eb25339cc83d5d5898d
HarishBabuAnkalla <129408270+Harish9440@users.noreply.github.com>
2023-04-14 11:47:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import History from './components/History'
+import History from './History'
 import './App.css'
 
 // These are the list used in the application. You can move them to any component needed.
